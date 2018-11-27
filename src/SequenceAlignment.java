f55fa80dad8e19c00b1df7906d9dcf9dfd8ac5fe
JinWonLee95 <dlwlsdnjs456@naver.com>
2018-11-27 17:22:10 +0900

    출력문 수정

--- src/SequenceAlignment.java
+++ src/SequenceAlignment.java
@@ -2,6 +2,8 @@ public class SequenceAlignment {
 
     static Character[] x = {' ', 'a', 'c', 'a', 'g', 'a', 'a', 'g', 't', 'a'}; // 짧은 염기서열
     static Character[] y = {' ', 'a', 'c', 't', 'g', 'a', 'g', 't', 't', 'a', 'a'}; // 긴 염기서열 (기준)
+    //    static Character[] y = {' ','g','a','a','c','g'};
+//    static Character[] x = {' ','g','a','c','t'};
     static int lx = x.length; // 공백을 직접 넣어줘 염기서열 길이+1
     static int ly = y.length; // 공백을 직접 넣어줘 염기서열 길이+1
 
@@ -14,9 +16,9 @@ public class SequenceAlignment {
     public static void sequence_Alignment(int lx, int ly) {
 
         /* a가 -의 ascii(45)로 채워짐 */
-        for(int i=0; i<lx; i++){
-            for(int j=0; j<ly; j++){
-                a[i][j]= '-';
+        for (int i = 0; i < lx; i++) {
+            for (int j = 0; j < ly; j++) {
+                a[i][j] = '-';
             }
         }
 
@@ -42,7 +44,7 @@ public class SequenceAlignment {
                 int max = a[i][j - 1] + q > a[i - 1][j - 1] + p[i][j] ? a[i][j - 1] + q : a[i - 1][j - 1] + p[i][j];
                 max = max > a[i - 1][j] + q ? max : a[i - 1][j] + q;
                 a[i][j] = max;
-                print(i*j);
+                print(i * j);
 
                 /* 어디서 온 data 인지 확인, 같은 값이 있으면 두개 다 추가 */
                 if (max == a[i][j - 1] + q) {
@@ -69,16 +71,14 @@ public class SequenceAlignment {
         }
 
         /*출력 부분*/
+        while (true) {
+            /* 기준 염기서열 */
+            System.out.print("기준의 염기서열 : \t");
+            for (int i = 1; i < ly; i++)
+                System.out.print(y[i] + "  ");
+            System.out.println();
 
-        /* 기준 염기서열 */
-        System.out.print("기존의 염기서열 : \t");
-        for (int i = 1; i < ly; i++)
-            System.out.print(y[i]+"  ");
-        System.out.println();
-
-
-        /* 비교할 염기서열 */
-        while(true){
+            /* 비교할 염기서열 */
             int sameCount = 0; // 같은 max 값이 있을 경우 또 출력해주기 위함
             System.out.print("비교된 최적조합 : \t");
             int templx = lx - 1, temply = ly - 1;
@@ -88,26 +88,26 @@ public class SequenceAlignment {
                 if (dataFrom[templx][temply] == 0) {
                     stringBuffer.append("  -");
                     /* 다른 같은 max 값이 존재한다면 */
-                    if(sameExist[templx][temply] != 0) {
+                    if (sameExist[templx][temply] != 0) {
                         dataFrom[templx][temply] = sameExist[templx][temply];
                         sameCount++;
-                        sameExist[templx][temply] =0;
+                        sameExist[templx][temply] = 0;
                     }
                     temply--;
                 } else if (dataFrom[templx][temply] == 1) {
-                    stringBuffer.append("  "+x[templx]);
+                    stringBuffer.append("  " + x[templx]);
                     /* 다른 같은 max 값이 존재한다면 */
-                    if(sameExist[templx][temply] != 0) {
+                    if (sameExist[templx][temply] != 0) {
                         dataFrom[templx][temply] = sameExist[templx][temply];
                         sameCount++;
-                        sameExist[templx][temply] =0;
+                        sameExist[templx][temply] = 0;
                     }
                     templx--;
                     temply--;
                 } else if (dataFrom[templx][temply] == 2) {
-                    stringBuffer.append("  "+x[templx]);
+                    stringBuffer.append("  " + x[templx]);
                     /* 다른 같은 max 값이 존재한다면 */
-                    if(sameExist[templx][temply] != 0) {
+                    if (sameExist[templx][temply] != 0) {
                         dataFrom[templx][temply] = sameExist[templx][temply];
                         sameCount++;
                         sameExist[templx][temply] = 0;
@@ -116,9 +116,11 @@ public class SequenceAlignment {
                 }
             }
             System.out.println(stringBuffer.reverse());
+            System.out.println("점수 : " + a[lx - 1][ly - 1] + "\n");
+
 
             //더이상 출력할 경우가 없으면 while문 빠져나가기
-            if(sameCount == 0){
+            if (sameCount == 0) {
                 break;
             }
             sameCount--; // 0이 아니면(아직 출력할게 남아 있으면) 하나를 줄여주고 다음 경우를 출력해줌
@@ -126,8 +128,8 @@ public class SequenceAlignment {
     }
 
     /* 행렬 출력문 */
-    public static void print(int k){
-        System.out.println("------------------------------------- "+k+"번째 계산 결과 -------------------------------------");
+    public static void print(int k) {
+        System.out.println("------------------------------------- " + k + "번째 계산 결과 -------------------------------------");
         System.out.print(" \t\t");
         for (int i = 0; i < ly; i++) {
             System.out.print(y[i] + "\t\t");
@@ -139,7 +141,7 @@ public class SequenceAlignment {
                 if (a[i][j] != 45) {
                     System.out.print(a[i][j] + "\t\t");
                 } else {
-                    System.out.print((char)a[i][j] + "\t\t");
+                    System.out.print((char) a[i][j] + "\t\t");
                 }
             }
             System.out.println();
